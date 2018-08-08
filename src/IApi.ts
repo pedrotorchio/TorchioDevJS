d08769d79f047465bcc0c0f457fd65c1b0a8c6b9
pedrotorchio <pedrotorchio@gmail.com>
2018-08-08 20:33:55 -0300

    creating basic api interface

--- src/IApi.ts
+++ src/IApi.ts
@@ -2,4 +2,12 @@ import * as Models from './index';
 
 export interface IApi{
   getAppInfo(): Models.AppInfo;
+  getAbout(): Models.About;
+  getWorks(): [Models.Work];
+  getServices(): [Models.Service];
+  getExperiences(): [Models.Experience];
+  getSkills(): [Models.Skill];
+  getEducations(): [Models.Education];
+  getLanguages(): [Models.Language];
+  getSocial(): [Models.Social];
 }
