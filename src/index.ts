a6e0d0c628c746eafde43491702bd53657aab8f6
pedrotorchio <pedrotorchio@gmail.com>
2018-08-09 11:48:21 -0300

    documenting procedure

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,30 @@ export * from './models/Language';
 export * from './models/Education';
 export * from './models/Social';
 
+export * from './fetcher/DirectusFetcher';
+export * from './Api';
 
-import { DirectusFetcher } from './fetcher/DirectusFetcher';
-new DirectusFetcher('http://api.torchiodev.com')
-.setAuthorizationHeader('Bearer 9gLLRfyNxBtQV392IVU4aUiPDNYsG20G')
-.getAppInfo();
 
+/**
+ * Procedure:
+ * 1 import Api
+ * 2 create fetcher instance (provided or created)
+ * 3 set authorization
+ * 4 create api instance
+ * 5 call methods
+ * 
+ * let fetcher = new DirectusFetcher('http://api.torchiodev.com')
+ *    .setAuthorizationHeader('Bearer 9gLLRfyNxBtQV392IVU4aUiPDNYsG20G')
+ * let api = new Api(fetcher )
+ *    .getAppInfo()
+ *    .then(appinfo => console.dir(appinfo.main_image.srcset));
+ */
+
+// import {DirectusFetcher} from './fetcher/DirectusFetcher';
+// import {Api} from './Api';
+
+// let fetcher = new DirectusFetcher('http://api.torchiodev.com')
+//    .setAuthorizationHeader('Bearer 9gLLRfyNxBtQV392IVU4aUiPDNYsG20G')
+// let api = new Api(fetcher )
+//    .getAppInfo()
+//    .then(appinfo => console.dir(appinfo));
