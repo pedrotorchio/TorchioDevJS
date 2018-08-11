3c7d788f0765850e2254e1799a3ee8d665f0ba88
pedrotorchio <pedrotorchio@gmail.com>
2018-08-11 11:42:01 -0300

    including Model synonyms

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -4,6 +4,7 @@ export class Model{
   
   sort: number = 0; // item order
   tags: Tags = new Tags(); // meta tags
+  synonyms: Tags = new Tags();
   description: string; // meta description
 
   constructor(id:number|string) {}
