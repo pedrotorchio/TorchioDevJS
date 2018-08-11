e58041617413034fea774cac660b0349a2a36def
pedrotorchio <pedrotorchio@gmail.com>
2018-08-11 12:23:55 -0300

    extracting Model copy functionality to base class

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -7,5 +7,20 @@ export class Model{
   synonyms: Tags = new Tags();
   description: string; // meta description
 
-  constructor(id:number|string) {}
+  constructor(public id:number|string) {}
+
+  static copy <T extends Model>(model:Model, Type:new (id:number|string)=>T): T {
+    let copy = new Type(model.id);
+    
+    if (model.sort)
+      copy.sort =  model.sort;
+    if (model.tags)
+      copy.tags =  model.tags;
+    if (model.synonyms)
+      copy.synonyms =  model.synonyms;
+    if (model.description)
+      copy.description =  model.description;
+
+    return copy;
+  }
 }
