51009313eab9c73fb740a252c1017d1e5eeeffc5
pedrotorchio <pedrotorchio@gmail.com>
2018-08-17 11:16:45 -0300

    build

--- dist/models/utils/Procedures.d.ts
+++ dist/models/utils/Procedures.d.ts
@@ -1,3 +1,3 @@
-import { Model, Image } from "js-models-helper/src";
+import { Model, Image } from "js-models-helper";
 export declare function data2model(data: any): Model;
 export declare function data2image(data: any, baseUrl?: string): Image;
--- dist/models/utils/Procedures.js
+++ dist/models/utils/Procedures.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const src_1 = require("js-models-helper/src");
+const js_models_helper_1 = require("js-models-helper");
 const joiner = require("url-join"); // preventing untyped module errors
 function data2model(data) {
-    let model = new src_1.Model(data.id);
+    let model = new js_models_helper_1.Model(data.id);
     // which default primitive value meta to extract
     for (const metaName of ["sort", "description"]) {
         if (data[metaName])
@@ -12,21 +12,21 @@ function data2model(data) {
     // which default Tag instance meta to extract
     for (const tagName of ["tags", "synonyms"]) {
         if (data[tagName])
-            model.meta[tagName] = new src_1.Tags(data[tagName]);
+            model.meta[tagName] = new js_models_helper_1.Tags(data[tagName]);
     }
     return model;
 }
 exports.data2model = data2model;
 function data2image(data, baseUrl = "") {
-    let main_image = data2model(data).copyInto(src_1.Image);
+    let main_image = data2model(data).copyInto(js_models_helper_1.Image);
     main_image.title = data.title;
     main_image.name = data.name;
     main_image.width = data.width;
     main_image.height = data.height;
     main_image.main_url = joiner(baseUrl, data.url);
     main_image.meta.description = data.caption;
-    main_image.meta.tags = new src_1.Tags(data.tags);
-    main_image.meta.tags = new src_1.Tags(data.tags);
+    main_image.meta.tags = new js_models_helper_1.Tags(data.tags);
+    main_image.meta.tags = new js_models_helper_1.Tags(data.tags);
     [160, 240, 320, 480, 640, 800, 960, 1080, 1240, 1440, 1600].forEach(size => main_image.addSource({
         url: joiner(baseUrl, "thumbnail", `${size}/${size}/contain`, data.name),
         size: ["width", size]
